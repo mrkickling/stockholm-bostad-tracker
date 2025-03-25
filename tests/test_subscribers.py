c91568fc3d412e869a87bc61d96f8da9d1ea27cc
Joakim Loxdal <loxdal@kth.se>
2025-03-25 11:34:20 +0100

    Remove old tests

--- tests/test_subscribers.py
+++
@@ -1,8 +0,0 @@
-from bostad_scraper.subscribers import load_subscribers_from_file
-
-def test_load_subscribers_from_file():
-    subscribers = load_subscribers_from_file(
-        "tests/subscribers.json"
-    )
-    assert len(subscribers) == 1
-    assert subscribers[0].email == 'email@example.com'
